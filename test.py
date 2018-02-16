dad74856c52f98c98d6b9f7294c0ea8b157a2784
SeungHyun Cha <lesktyu@gmail.com>
2018-02-16 22:45:11 +0900

    180217 Commit

---
+++ test.py
@@ -0,0 +1,85 @@
+import random
+import sys
+
+"""
+msg = 'Life is Short!'
+print(msg.split())
+
+print(1, 2, 3)
+
+print('My name is', 'Cha')
+
+# input #
+age = input('Your age?')
+print(age)
+
+# variable #
+var = 1+3j
+var *= 3
+print(var)
+
+# test #
+#1python = 1
+#print(1python)
+print("Hello")
+print('Hello')
+
+my_bool = False
+
+# if #
+if my_bool == True:
+	print(my_bool)
+elif my_bool == False:
+	print(my_bool)
+
+# for #
+for a in [1,2,3]:
+	print(a)
+
+# while #
+
+i = 0
+while i<4:
+	i+=1
+	print(i)
+
+# function # 
+
+def sum(a,b):
+	return a+b
+
+print(sum('a','b'))
+
+# list #
+
+my_list = [1,2,3]
+students = ['cha', 'kim', 'choi']
+
+print(my_list)
+print(students)
+print(random.choice(students))
+students.append('lee')
+print(students)
+
+students[0] = 'chacha'
+print(students)
+
+# tuple does not change value #
+my_tuple = ('cha', 'lee', 'choi')
+print(my_tuple)
+
+# dictionary #
+my_dict = {'cha': 'm', 'lee': 'w'}
+print(my_dict['cha'])
+"""
+
+# test #
+
+my_str = 'coding'
+print(list(my_str))
+
+my_num = 1+2j
+print(my_num.real)
+print(my_num.imag)
+print(my_num.conjugate())
+print(abs(my_num))
