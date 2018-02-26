249d1f862e9f6544a3c36a76451af35715625084
echaseu <echaseu@KR00200510.ericsson.se>
2018-02-26 17:38:47 +0900

    Revise and add class file

--- Class/180226/Class.py
+++ Class/180226/Class.py
@@ -5,26 +5,38 @@ Created on 2018. 2. 26.
 '''
 
 class Calculator:
-    def setdate(self, first, second):
+    def __init__(self, first, second):
         self.first = first
         self.second = second
-    
-    def sum(self, first, second):
-        return first+second
-    
-    def mul(self, first, second):
-        return first*second
-    
-    def sub(self, first, second):
-        return first-second      
-    
-    def div(self, first, second):
-        return first/second
+#    def setdate(self, first, second):
+#        self.first = first
+#        self.second = second    
+    def sum1(self, first, second):
+        return self.first + self.second
+    def mul1(self, first, second):
+        return self.first * self.second
+    def sub1(self, first, second):
+        return self.first - self.second      
+    def div1(self, first, second):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
 
-a = Calculator()
+class Morefunc(Calculator):
+    def pow1(self):
+        return self.first ** self.second
+    
+a = Calculator(4,5)
+b = Morefunc(4,0)
+#a.setdate(4,5)
 
-a.setdate(4,5)
+print a.sum1(a.first, a.second)
 
-print a.sum(a.first,a.second)
+print b.sum1(b.first, b.second)
+#print b.sum1()
+print b.pow1()
+print b.div1(b.first, b.second)
+#print a.sum1()
 #print a.mul(a.first,a.second)
 
