005b54efce9d7a940e59829511ef6f4e438e57a9
echaseu <echaseu@KR00200510.ericsson.se>
2018-03-20 13:47:04 +0900

    180316 create mo, attr class

--- MOM/MyWork/MOMParser.py
+++ MOM/MyWork/MOMParser.py
@@ -1,34 +1,68 @@
 import xml.etree.ElementTree as ET
-
-class MomParser():
+  
+class MomParser:
     def __init__(self, name):
         self.name = name
         self.ns = {}
+        self.weight = 0
         self.root = None
         for event, elem in ET.iterparse(name, events=('start','end')):
-            if event == 'start':
-                if elem.tag == 'attribute':           
-                # 'start' and 'end' mean '<', '>' in XML 
-                # elem.text is text <element> text </element> in XML
-                # tag is the name of element
-                # elem.attrib is <element name /> in XML
-                # elem._children is child of this element
-                    print event, elem
-                    print elem.tag
-                    print type(elem.tag)
-                    print elem.attrib
-                    print type(elem.attrib)
-                    print elem._children
-                    print type(elem._children)
-                    if elem.text == None: pass
-                    else: print elem.text
+            #print elem.tag
+            if event == 'start' and self.root == None:              
+                self.root = elem.tag                                                 
+                #print self.root
+            elif event == 'start':
+                if elem.tag == 'class':
+                    #print elem.attrib.values()[0]
+                    moName = elem.attrib.values()[0]
+                    mo = MO(moName)         
+                    for child in elem:
+                        #print child.tag
+                        if child.tag == 'description':
+                            mo.description = child.text
+                        else:
+                            pass
+                    print mo.__dict__
+                     
+                elif elem.tag == 'attribute':
+                    attrName = elem.attrib.values()[0]
+                    attr = ATTR(attrName)
+                          
             else:
                 pass
-                #print event, elem
-                   
+            
+'''
+ temp = []
+    for child in elem:
+        #print child.tag
+        if child.tag == 'description':
+            
+        else:
+            if child.tag == 'attribute':
+                
+        temp.append(child.tag)
+    obj.attribute = temp
+'''
+class MO:
+    def __init__(self, name):
+        self.name = name
+        self.attribute = None
+        self.description = None
+        self.parents = None
+        self.child = None
+    
+    def __repr__(self):
+        return 
+    
+class ATTR:
+    def __init__(self, name):
+        self.name = name
+        self.attribute = None
+        self.description = None
+        self.mo = None
+
 if __name__ == '__main__':
     #name = "LteRbsNodeComplete_Itr27_R10D03.xml"
     name = "sample.xml"
     parser = MomParser(name)
-#   print parser.root.__dict__
     #print parser.ns    
