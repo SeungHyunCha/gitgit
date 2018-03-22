89ce301c72d7f4a4e22c800ccd7e50cffa4d6d17
echaseu <echaseu@KR00200510.ericsson.se>
2018-03-22 17:56:15 +0900

    180322 split func

--- MOM/MyWork/MOMParser.py
+++ MOM/MyWork/MOMParser.py
@@ -1,88 +1,118 @@
 import xml.etree.ElementTree as ET
 
-class MomParser(object):
+class MomParser:
+    TAG = ('class', 'attribute', 'enum', 'struct', 'exception')
     def __init__(self, name):
         self.name = name
-        self.ns = {}
         self.root = None
-        for event, elem in ET.iterparse(name, events=('start', 'end')):
+        self.whole_mo = {}
+        self.whole_attr = {}
+        self.__run()
+        
+    def __run(self):
+        for event, elem in ET.iterparse(self.name, events=('start', 'end')):
             if event == 'start' and self.root == None:              
                 self.root = elem.tag                                                
             elif event == 'start': 
                 if elem.tag == 'class':
-                    mo = MO(elem.attrib.values()[0], elem) 
-                    mo.handle()
-                #elif elem.tag == 'enum': pass
-                #elif elem.tag == 'struct': pass
-                #elif elem.tag == 'exception': pass
+                    mo = Mo(elem)
+                    self.whole_mo[mo.getName()] = mo.addMo()
+                elif elem.tag == 'enum': pass
+                elif elem.tag == 'struct': pass
+                elif elem.tag == 'exception': pass
                 else: pass
             elif event == 'end':
                 if elem.tag == 'class': 
                     for mo_child in elem:  
-                        if mo_child.tag == 'attribute':  
-                            attr = ATTR(mo_child.attrib.values()[0], mo_child)  
-                            attr.handle()
-                            attr.mo = elem.attrib.values()[0]
-                            mo.attribute.append(attr.name)
-                            #print 'Attribute', attr.__dict__
+                        if mo_child.tag == 'attribute': 
+                            attr = Attr(mo_child)  
+                            self.whole_attr[attr.getName()] = attr
+                            attr.mo = mo.getName()
+                            mo.addAttrs(attr)
                         else: pass
-                    print 'MO', mo.__dict__
+                    print mo.__dict__
                 else: pass         
             else: pass
+    
+    def printElem(self, mo="", attribute=""):
+        pass
 
-class MO(object):
-    def __init__(self, name, elem):
-        self.name = name
-        self.attribute = []
-        self.description = None
+class Mo:
+    def __init__(self, elem):
+        self.name = elem.attrib.values()[0]
+        self.attrs = {}
         # self.parents = None
         # self.child = None
         self.elem = elem
-        
-    def handle(self):
+        self.__handle()
+    
+    def getName(self):
+        return self.name
+    
+    def addMo(self):
+        return self.elem
+    
+    def getTag(self):
+        return self.elem.tag
+            
+    def addAttrs(self, attr):
+        self.attrs[attr.getName()] = attr
+    
+    def getAttrs(self): #get attrs
+        return self.attrs
+    
+    def getAttrsinfo(self, attr):
+        for key, value in attr.__dict__.items(): #get infomation of attrs
+            self.attrs[key] = value
+    
+    def __str__(self):
+        return 'MOC: ' + self.name
+    
+    def __handle(self):
         for mo_child in self.elem:
             if len(mo_child._children) == 0 and mo_child.tag != 'attribute':
                 if mo_child.text == None: exec("self.%s = 'on'" % mo_child.tag)
                 else: exec("self.%s = %r" % (mo_child.tag, mo_child.text))
-            else: pass#print 'error_MO_handle', mo_child.attrib
-    '''    
-    def __getattribute__(self, attr):
-        # print "get attr %s" % attr
-        return object.__getattribute__(self, attr)
-    def __setattr__(self, attr, val):
-        # print "set attr %s to %r" % (attr, val)
-        return object.__setattr__(self, attr, val)
-    '''
+            else: pass#print 'error_Mo_handle', mo_child.attrib
     
-class ATTR(object):
-    def __init__(self, name, elem):
-        self.name = name
-        self.description = None
-        self.mo = None
+class Attr:
+    def __init__(self, elem):
+        self.name = elem.attrib.values()[0]
         self.elem = elem
-        self.dataType = None
+        self.mo = None
+        self.types = {}
+        self.__handle()
+    
+    def getName(self):
+        return self.name
+    
+    def getTag(self):
+        return self.elem.tag
+    
+    def __str__(self):
+        return 'Attribute: ' + self.name
         
-    def handle(self):
+    def __handle(self):
         for attribute in self.elem:  
             if len(attribute._children) == 0:  
-                if attribute.text == None: exec "self.%s = 'on'" % attribute.tag  
+                if attribute.text == None: exec "self.%s = 'on'" % attribute.tag
                 else: exec "self.%s = %r" % (attribute.tag, attribute.text) 
             else:
                 if attribute.tag == 'dataType':
-                    temp = attribute._children[0].tag
-                    Type = DataType(temp, attribute)
-                    Type.handle()               
-                    self.dataType = Type.name
-                    print 'DataType', Type.__dict__
-                else: 'error_ATTR_handle_datatype'   
+                    Type = DataType(attribute)        
+                    for key, value in Type.__dict__.items(): #get dataType
+                        self.types[key] = value
+                else: 'error_Attr_handle_datatype'   
 
-class DataType(object):
-    def __init__(self, name, elem):
-        self.name = name
+class DataType:
+    def __init__(self, elem):
+        self.name = None
         self.elem = elem
-        
-    def handle(self):
+        self.__handle()
+
+    def __handle(self):
         for dtype in self.elem:
+            self.name = dtype.tag
             if dtype.attrib == {}:  
                 for child in dtype:
                     if len(child._children) == 0:
@@ -108,7 +138,8 @@ class DataType(object):
                     exec "self.%s.update({%r:%r})" % (dtype.tag, child.tag, child.text)
                          
 if __name__ == '__main__':
-    #name = "LteRbsNodeComplete_Itr27_R10D03.xml"
-    name = "sample.xml"
+    name = "LteRbsNodeComplete_Itr27_R10D03.xml"
+    #name = "sample.xml"
     parser = MomParser(name)
-    # print parser.ns    
+    #parser.show();
+
