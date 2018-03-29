08b5cf638e337e64f0021d4138843a45101a7ea3
echaseu <echaseu@KR00200510.ericsson.se>
2018-03-29 13:47:41 +0900

    180329 print info

--- MOM/MyWork/MOMParser2.py
+++ MOM/MyWork/MOMParser2.py
@@ -1,6 +1,4 @@
 import xml.etree.ElementTree as ET
-from __builtin__ import str
-from _ast import Str
 
 class MomParser:
     def __init__(self, name):
@@ -14,49 +12,76 @@ class MomParser:
             if event == 'start': # create obj for mo, enum, struct, exception
                 if elem.tag == 'class':
                     moname = Mo(elem)
-                    self.mos[moname.getName()] = elem
+                    self.mos[moname.getName()] = moname
+                elif elem.tag == 'enum':
+                    enname = Enum(elem)
+                elif elem.tag == 'struct':
+                    structname = Struct(elem)
+                elif elem.tag == 'exception': 
+                    excepname = Exception(elem)
+                else: pass    
+
             elif event == 'end': # add infomation in obj
                 for attr in elem: 
                     if elem.tag == 'class' and attr.tag == 'attribute':
                         child = Attr(attr)
                         child.mo = moname.getName()
+                        self.attrs[child.getName()] = child
                         moname.addAttrs(child)
                         moname.addAttrsInfo(child)
+                    
+                    elif elem.tag == 'enum' and attr.tag == 'enumMember': 
+                        child = Attr(attr)  
+                        enname.addAttrs(child)
+                        
+                    elif elem.tag == 'struct' and attr.tag == 'structMember': 
+                        child = Attr(attr)  
+                        structname.addAttrs(child)
+                    
+                    elif elem.tag == 'exception' and attr.tag == 'exceptionParameter': 
+                        child = Attr(attr)  
+                        excepname.addAttrs(child)
+                    else: pass
+                    
             else: pass
 
-    def printMo(self, mo=""):
-        str = "-" * 100
-        print str + "print MO" + str
+    def mom(self, mo="", attr=""): # print element
         str = "*" * 100
         mo_list = self.mos.keys()
-        if mo in mo_list:
-            temp = Mo(self.mos[mo])
-            print str
-            print "mo\t\t", "attribute\t\t"
-            print str
-            print temp.getName()
-            print str
-            print temp.getAttrs()
-        elif mo == "":
-            for key in mo_list:
-                temp = Mo(self.mos[key])
-                print str
-                print temp.getName()
-
+        if attr == "":
+            if mo in mo_list: # check mo
+                getMo = self.mos[mo]
+                getMo.showMoAttrInfo()
+    
+            elif mo == "":
+                print str,'\n',"MO\t\t\t",'\n',str
+                for mo in self.mos.keys():
+                    print mo
+                    
+            else:
+                print str,'\n',"MO\t\t\t",'\n',str
+                print str, '\n', "Attribute\t\t",'\n',str
+        
+        else:
+            if mo == "":
+                if attr in self.attrs:
+                    getAttr = self.attrs[attr]
+                    print getAttr.printAttr()
+            else: pass
+    
 class Mo:
     def __init__(self, elem):
         self.name = elem.attrib.values()[0]
         self.attrs_obj = {}
         self.attrs_info = {}
+        self.description = None
+        self.flags = []
         self.obj = elem
         self.handle()
     
     def getName(self):
         return self.name
     
-    def getMo(self):
-        return self.obj
-            
     def addAttrs(self, attr):
         self.attrs_obj[attr.getName()] = attr
     
@@ -64,8 +89,8 @@ class Mo:
         return self.attrs_obj
     
     def addAttrsInfo(self, attr):
-        for key, value in attr.__dict__.items(): #get infomation of attrs
-            self.attrs_info[key] = value
+#         for key, value in attr.__dict__.items(): #add infomation of attrs
+        self.attrs_info[attr.getName()] = attr.__dict__.items()
     
     def getAttrsInfo(self):
         return self.attrs_info
@@ -76,23 +101,38 @@ class Mo:
     def handle(self):
         for mo_child in self.obj:
             if len(mo_child._children) == 0 and mo_child.tag != 'attribute' or 'enumMember' or 'structMember':
-                if mo_child.text == None: exec("self.%s = {}" % mo_child.tag)
+                if mo_child.text == None: 
+                    exec("self.%s = {}" % mo_child.tag)
+                    self.flags.append(mo_child.tag)
                 else: 
                     if mo_child.text == '\n\t\t\t\t': pass
                     else: exec("self.%s = %r" % (mo_child.tag, mo_child.text))
             else: pass
-            
+    
+    def showMoAttrInfo(self):
+        str = "*" * 100
+        print str,'\n',"MO:\t\t", self.name
+        for flag in self.flags: print flag
+        if self.description != None: print self.description
+        print str,'\n',"Attribute:"
+        for attr in self.attrs_obj.keys(): print "\t\t", attr
+       
 class Attr:
     def __init__(self, elem):
         self.name = elem.attrib.values()[0]
         self.obj = elem
+        self.description = None
         self.mo = None
         self.dataTypes = {}
+        self.flags = []
         self.__handle()
     
     def getName(self):
         return self.name
     
+    def getData(self):
+        return self.__dict__
+        
     def __handle(self):
             for attr in self.obj:
                 if len(attr._children) == 0:
@@ -103,7 +143,20 @@ class Attr:
                     for key, value in Type.__dict__.items(): #get dataType in Attr
                         if key == 'elem': pass
                         else: self.dataTypes[key] = value
-#                     print self.dataTypes
+
+    def printAttr(self):
+        print ""
+        str = "*" * 100
+        print str,'\n',"MO\t\t\t", "Attribute\t\t", '\n', str
+        print "%s\t\t%s\t\t" % (self.mo, self.name)
+        print str
+        for key, value in self.__dict__.items():
+            if key == 'obj' or key == 'name' or key == 'mo': pass
+            elif value == {}:
+                self.flags.append(key)
+            else: 
+                print "%s\t\t%s" %(key, value)
+            
         
 class DataType:
     def __init__(self, elem):
@@ -148,19 +201,16 @@ class DataType:
                     temp[child.tag] = child.text
                     exec "self.%s.update(%s)" % (dtype.tag, temp)
                     
-class Enum(Mo):
-    pass
-class Struct(Mo):
-    pass
-class Exception(Mo):
-    pass
+class Enum(Mo): pass
+class Struct(Mo): pass
+class Exception(Mo): pass
 
                        
 if __name__ == '__main__':
     name = "LteRbsNodeComplete_Itr27_R10D03.xml"
     #name = "sample.xml"
     parser = MomParser(name)
-#     parser.printElem()
-#     print parser.allattr
-    print parser.printMo('MceFunction')
+#     parser.mom()
+    parser.mom(mo='ReportConfigA1Sec')
+    parser.mom(attr='a1ThresholdRsrpSec')
 
