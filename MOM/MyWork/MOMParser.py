5793a1e611d25ae4571f3fc714ecf7d9e3034fb9
echaseu <echaseu@KR00200510.ericsson.se>
2018-04-02 19:31:38 +0900

    180402 revising error

--- MOM/MyWork/MOMParser.py
+++ MOM/MyWork/MOMParser.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import re
+from test.test_codeccallbacks import NoEndUnicodeDecodeError
 
 class MomParser:
     def __init__(self, name):
@@ -51,8 +52,7 @@ class MomParser:
         if mo == "":    
             print str,'\n',"MO\t\t\t",'\n',str
             for mo in self.mos.keys():
-                print mo
-            
+                print mo   
         else:
             p = re.compile(mo, re.IGNORECASE)
             mo_list = self.mos.keys()
@@ -60,13 +60,14 @@ class MomParser:
                 check = p.search(moc)
                 if check:   
                     getMo = self.mos[moc]
+#                     print getMo.getTag()
                     if attr == "":
                         getMo.showMoAttrInfo()
                     else:
                         print str,'\n',"Mo\t\t\tAttribute"
                         print getMo.getName()
                         m = re.compile(attr, re.IGNORECASE)
-                        attr_list = getMo.getAttrs()   
+                        attr_list = getMo.getAttrs()
                         for attr_name in sorted(attr_list):
                             check1 = m.search(attr_name)
                             if check1:
@@ -85,6 +86,9 @@ class Mo:
     
     def getName(self):
         return self.name
+    
+    def getTag(self):
+        return self.obj.tag
     
     def addAttrs(self, attr):
         self.attrs_obj[attr.getName()] = attr
@@ -130,114 +134,168 @@ class Attr:
         self.mo = None
         self.type = None
         self.flags = []
+        self.length = {}
+        self.range = {}
+        self.values = {}
         self.others = {}
         self.__handle()
     
     def getName(self):
         return self.name
     
-    def getData(self):
-        return self.__dict__
+    def getAttrFlag(self):
+        return sorted(self.flags)
+    
+    def detAttrDatatype(self):
+        return self.type.printData()
     
     def __handle(self):
             for attr in self.obj:
                 if len(attr._children) == 0:
                     if attr.text == None: 
-                        exec "self.%s = {}" % attr.tag
                         self.flags.append(attr.tag)
                     else: 
                         exec "self.%s = %r" % (attr.tag, attr.text)
                         self.others.update({attr.tag:attr.text})
                 else:
-                    self.type = DataType(attr)
-#                     self.type.printData()                    
-#                     for key, value in Type.__dict__.items(): #get dataType in Attr
-#                         if key == 'elem': pass
-#                         else: self.dataTypes[key] = value
-
-    def printAttr(self):
-        str = "-" * 100
-        other_list = self.others.keys()
-        print "\t\t\t", self. name
-        print "\t\t\t\t\t",
-        for flag in sorted(self.flags): print flag,
-        print '\n', 
-        for key in sorted(other_list): print "\t\t\t\t\t<%s>\n\t\t\t\t\t%s" %(key, self.others[key])
-        self.type.printData()
-        print '\n', str
+                    if attr.tag == 'dataType':
+                        self.type = DataType(attr)
+                    else:
+                        if attr.attrib == {}: 
+                            self.type = attr.tag
+                            if self.type == 'sequence':
+                                for child in attr:
+                                    if child.text == None: 
+                                        print attr, child.tag
+                                    else:
+                                        if child._children == []:
+                                            self.length.update({child.tag:child.text})
+                                        else:
+                                            if child.tag == 'long':
+                                                self.type = child.tag
+                                                for gchild in child:
+                                                    if gchild._children == []:
+                                                        self.values.update({gchild.tag:gchild.text})
+                                                    else:
+                                                        for ggchild in gchild:
+                                                            self.range.update({ggchild.tag:ggchild.text})
+                                            else: 
+                                                temp = []
+                                                for gchild in child:
+                                                    temp.append(gchild.text)
+                                                self.values.update({child.tag:temp})
+                            else:
+                                for child in attr:
+                                    if child.attrib == {}:
+                                        if child.tag == 'range':
+                                            for key in child:
+                                                self.range.update({key.tag:key.text})
+                                        elif child.tag == 'lengthRange':
+                                            for key in child:
+                                                self.length.update({key.tag:key.text})
+                                        else: 
+                                            if child._children == []:
+                                                self.values.update({child.tag:child.text})
+                                            else:
+                                                print 'attr __handle', child.tag, child.text
+                                    else: 
+                                        print 'attr __handle', child.tag, child.text
+                        else:
+                            pass
+#     def printAttr(self):
+#         str = "-" * 100
+#         other_list = self.others.keys()
+#         print "\t\t\t", self. name
+#         print str
+# #         print '<flag>',
+# #         for flag in sorted(self.flags): print flag,
+#         print '\n', 
+# #         for key in sorted(other_list): print "%s %s" %(key, self.others[key]) // show attr description
+#         if self.type == None: pass
+#         else: self.type.printData()
+#         print '\n', str
         
 class DataType:
     def __init__(self, elem):
         self.elem = elem
-#         self.values = []
+        self.flags = []
+        self.type = None
+        self.length = {}
+        self.range = {}
+        self.values = {}
         self.__handle()
-
+    
+    def getData(self):
+        return self.__dict__
+    
     def __handle(self):
+        temp = {}
         if self.elem.attrib == {}: pass
         else:
             exec "self.%s = %r" % (self.elem.attrib.keys()[0], self.elem.attrib.values()[0]) 
         for dtype in self.elem:
             exec "self.%s = {}" % dtype.tag
+            self.type = dtype.tag
             if dtype.attrib == {}:
                 for child in dtype:
                     if len(child._children) == 0:
                         if child.attrib == {}: # get min,max values
                             if child.text == None: 
-                                exec "self.%s = {}" % child.tag
+                                if dtype.tag == 'sequence': 
+                                    self.type = dtype.tag
+                                else: self.flags.append(dtype.tag)
+#                                 exec "self.%s = {}" % child.tag
                             else:
-#                                 self.values.append(child.text)
-                                temp = {}
                                 temp[child.tag] = child.text
                                 exec "self.%s.update(%s)" % (dtype.tag, temp)
-                        else:
+#                                 print dtype.tag, child.tag, temp
+                        else: # Ref elem
                             if child.text == None: 
                                 exec "self.%s = %r" % (child.tag, child.attrib.values()[0])
-                            else: pass
+                            else: print 'error', self.elem, dtype.tag
                     else:
                         exec "self.%s = {}" % child.tag
                         for gchild in child:
                             if gchild.text == '\n\t\t\t\t\t\t\t':
-                                for ggchild in gchild:
-#                                     self.values.append(ggchild.text)
-                                    temp = {}
+                                for ggchild in gchild: # min, max value
                                     temp[ggchild.tag] = ggchild.text
                                     exec "self.%s.update(%s)" % (child.tag, temp)
                             else:
-#                                 self.values.append(gchild.text)
-                                temp = {}
                                 temp[gchild.tag] = gchild.text
                                 exec "self.%s.update(%s)" % (child.tag, temp)
                             
             else: 
-                exec "self.%s = %s" % (dtype.tag, dtype.attrib)
+                exec "self.%s = {%r}" % (dtype.tag, dtype.attrib.values()[0]) # Ref elem
                 for child in dtype:
-#                     self.values.append(child.text)
-                    temp = {}
                     temp[child.tag] = child.text
                     exec "self.%s.update(%s)" % (dtype.tag, temp)
                     
     def printData(self):
-        print "\t\t\t\t\t<dataType>"
-        key_list = self.__dict__.keys()
-        for key in key_list:
-            if key != 'elem':
-                if self.__dict__[key] == {}:
-                    print "\t\t\t\t\t", key
-                else:
-                    child = self.__dict__[key]
-                    if key == None: pass
-                    else:
-                        if key == 'range':
-                            print "\t\t\t\t\t", key,
-                            print 'min:', child['min'], 'max:', child['max']
-                        elif key == 'enumRef':
-                            for cchild in child:
-                                if cchild == 'name': print "\t\t\t\t\t", key, str(child[cchild])
-                                else: print "\t\t\t\t\t", cchild, str(child[cchild])    
-                        else:
-                            print "\t\t\t\t\t", key
-                            for cchild in child:
-                                print "\t\t\t\t\t", cchild, str(child[cchild])    
+        key_list = self.__dict__
+#         print key_list
+#         print 'type', self.type
+        for val in self.__dict__:
+            if val == self.type: 
+                values = self.__dict__[val]
+                for name in values:
+                    self.values.update({name:values[name]})
+            else: pass 
+                
+#         print self.values
+        for key in key_list: pass
+#                     child = self.__dict__[key]
+#                     if key == 'range':
+#                         print "\t", key,
+#                         print 'min:', child[r'min*'], '^max:', child[r'max*']
+#                     elif key == 'enumRef' or 'structRef':
+#                         print key, child
+# #                         for cchild in child:
+# #                             if cchild == 'name': print "\t", key, str(child[cchild])
+# #                             else: print "\t\t\t\t\t", cchild, str(child[cchild])    
+#                     else:
+#                         print "\t", key
+#                         for cchild in child:
+#                             print "\t", str(cchild), str(child[cchild])    
                     
                         
 class Enum(Mo): pass
@@ -247,11 +305,17 @@ class MyException(Mo): pass
                        
 if __name__ == '__main__':
     name = "LteRbsNodeComplete_Itr27_R10D03.xml"
+#     old_mom = """
+#     fdsafsa
+#     
+#     """
     #name = "sample.xml"
     parser = MomParser(name)
-#     parser.mom()
+#     print 'aaa'
+#     parser.mom(mo='Rcs')
 #     parser.mom(mo='ReportConfigA1Sec')
-    parser.mom(mo='utrancelltdd')
-#     parser.mom(mo='utrancelltdd', attr='zzz')
+#     parser.mom(mo='utrancelltdd')
+    parser.mom(mo='utrancelltdd', attr='r')
+#     print 'aaa'
 #     parser.mom(mo='ReportConfigA1Sec', attr='r')
 
