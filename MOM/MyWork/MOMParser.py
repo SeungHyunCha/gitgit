f164150185b0a711ec912b48713945e45e822f96
echaseu <echaseu@KR00200510.ericsson.se>
2018-04-03 17:16:32 +0900

    [task] solved the error

--- MOM/MyWork/MOMParser.py
+++ MOM/MyWork/MOMParser.py
@@ -1,12 +1,17 @@
 import xml.etree.ElementTree as ET
 import re
-from __builtin__ import isinstance
 
 class MomParser:
     def __init__(self, name):
         self.name = name
         self.mos = {}
         self.attrs = {}
+        self.enums = {}
+        self.emembers = {}
+        self.structs = {}
+        self.smembers = {}
+        self.exceps = {}
+        self.exmembers = {}
         self.__run()
         
     def __run(self):
@@ -16,11 +21,14 @@ class MomParser:
                     moname = Mo(elem)
                     self.mos[moname.getName()] = moname
                 elif elem.tag == 'enum':
-                    enname = Enum(elem)
+                    enumname = Enum(elem)
+                    self.enums[enumname.getName()] = enumname
                 elif elem.tag == 'struct':
                     structname = Struct(elem)
+                    self.structs[structname.getName()] = structname
                 elif elem.tag == 'exception': 
                     excepname = MyException(elem)
+                    self.exceps[excepname.getName()] = excepname
                 else: pass    
 
             elif event == 'end': # add infomation in obj
@@ -30,21 +38,26 @@ class MomParser:
                         child.mo = moname.getName()
                         self.attrs[child.getName()] = child
                         moname.addAttrs(child)
-                        moname.addAttrsInfo(child)
+#                         moname.addAttrsInfo(child)
                     
                     elif elem.tag == 'enum' and attr.tag == 'enumMember': 
-                        child = Attr(attr)  
-                        enname.addAttrs(child)
+                        child = Attr(attr)
+                        child.mo = enumname.getName()  
+                        self.emembers[child.getName()] = child
+                        enumname.addAttrs(child)
                         
                     elif elem.tag == 'struct' and attr.tag == 'structMember': 
-                        child = Attr(attr)  
+                        child = Attr(attr) 
+                        child.mo = structname.getName() 
+                        self.smembers[child.getName()] = child
                         structname.addAttrs(child)
                     
                     elif elem.tag == 'exception' and attr.tag == 'exceptionParameter': 
                         child = Attr(attr)  
+                        child.mo = excepname.getName()
+                        self.emembers[child.getName()] = child
                         excepname.addAttrs(child)
                     else: pass
-                    
             else: pass
 
     def mom(self, mo="", attr=""): # print element
@@ -54,7 +67,17 @@ class MomParser:
             for mo in self.mos.keys():
                 print mo   
             if attr != "":
-                pass
+                print str,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'defaultValue'.ljust(30), 'Flags'.ljust(30), 'Range'.ljust(30)
+                print str
+                p = re.compile(attr, re.IGNORECASE)
+                for attr_name in self.attrs:
+                    check = p.search(attr)
+                    if check:
+                        getAttr = self.attrs[attr_name]
+                        print getAttr.getMoName().ljust(30), getAttr.getName().ljust(30), getAttr.getValues(), getAttr.getFlags().ljust(30), getAttr.getRange()
+                    else:
+                        pass
+                        
         else:
             p = re.compile(mo, re.IGNORECASE)
             mo_list = self.mos.keys()
@@ -62,11 +85,10 @@ class MomParser:
                 check = p.search(moc)
                 if check:   
                     getMo = self.mos[moc]
-#                     print getMo.getTag()
                     if attr == "":
                         getMo.showMoInfo()
                     else:
-                        print str,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'defaultValue'.ljust(30), 'Flags'.ljust(30), 'Range'.ljust(30)
+                        print str,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'Flags'.ljust(30), 'length'.ljust(10), 'default'.ljust(10), 'Range'
                         print str
                         m = re.compile(attr, re.IGNORECASE)
                         attr_list = getMo.getAttrs()
@@ -74,7 +96,7 @@ class MomParser:
                             check1 = m.search(attr_name)
                             if check1:
                                 getAttr = attr_list[attr_name]
-                                print getAttr.mo.ljust(30), getAttr.getName().ljust(30), getAttr.getValues(), getAttr.getFlags().ljust(30), getAttr.getRange()
+                                print getAttr.mo.ljust(30), getAttr.getName().ljust(30), getAttr.getFlags().ljust(30), getAttr.getLength().ljust(10), getAttr.getValues().ljust(10), getAttr.getRange()
                             else:
                                 pass
                 else: pass
@@ -122,22 +144,22 @@ class Mo:
             else: pass
     
     def showMoInfo(self):
-        str = "*" * 100
-        print str,'\n', "MO", '\n', str 
+        str = "*" * 120
+        print str,'\n', "MOC", '\n', str 
         print self.name
         for flag in sorted(self.flags): print flag
         other_list = self.others.keys()
         for key in sorted(other_list): print "%s\t%s" %(key, self.others[key]) 
-        print str,'\n',"Attribute".ljust(30), "Type".ljust(35), 'Flags', '\n', str
+        print str,'\n',"Attribute".ljust(30), "Type".ljust(40), 'Flags', '\n', str
         for key, value in sorted(self.attrs_obj.items()): 
-            print key.ljust(30), value.getType(), ''.ljust(29), value.getFlags()
+            print key.ljust(30), value.getType().ljust(40), value.getFlags()
         
 class Attr:
     def __init__(self, elem):
         self.name = elem.attrib.values()[0]
         self.obj = elem
         self.mo = None
-        self.type = None
+        self.types = None
         self.flags = []
         self.length = {}
         self.range = {}
@@ -148,33 +170,37 @@ class Attr:
     def getName(self):
         return self.name
     
+    def getMoName(self):
+        return self.mo
+        
     def getFlags(self):
         self.flags.pop()
         str = ','.join(self.flags)
         return str
     
     def getType(self):
-        if isinstance(self.type, dict) == True:
-            print self.type
-            for key, value in self.type:
-                str = key
-            return str
-        else: return self.type
+        if self.types == {}:
+            return 
+        else: return str(self.types)
     
     def getLength(self):
-        return self.length
+        if self.length == {}:
+            return ''
+        else: return str(self.length)
     
     def getRange(self):
-        return self.range
+        if self.range == {}:
+            return ''
+        else: 
+            return str(self.range)
     
     def getValues(self):
-        return self.values
+        if self.values == {}:
+            return ''
+        else: return str(self.values)
     
     def getAttrFlag(self):
         return sorted(self.flags)
-    
-    def detAttrDatatype(self):
-        return self.type.printData()
     
     def __handle(self):
         for attr in self.obj:
@@ -192,21 +218,21 @@ class Attr:
                         self.length = data.getLength()
                         self.range = data.getRange()
                         self.values = data.getValues()
-                        self.type = data.getType()
+                        self.types = data.getType()
                 else:
                     data = DataType(attr)  
                     self.flags.append(data.getFlags())
                     self.length = data.getLength()
                     self.range = data.getRange()
                     self.values = data.getValues()
-                    self.type = data.getType()
+                    self.types = data.getType()
                 
         
 class DataType:
     def __init__(self, attr):
         self.attr = attr
         self.flags = []
-        self.type = None
+        self.types = None
         self.length = {}
         self.range = {}
         self.values = {}
@@ -219,7 +245,7 @@ class DataType:
         return self.flags
     
     def getType(self):
-        return self.type
+        return self.types
     
     def getLength(self):
         return self.length
@@ -232,25 +258,25 @@ class DataType:
     
     def __handle(self):
         if self.attr.attrib == {}: 
-            self.type = self.attr.tag
-            if self.type == 'sequence':
+            self.types = self.attr.tag
+            if self.types == 'sequence':
                 for child in self.attr:
                     if child.text == None:
                         if child.tag == 'nonUnique':
                             self.flags = child.tag
                         else: 
                             if child.attrib == {}:
-                                self.type = child.tag
+                                self.types = child.tag
                             else:
                                 temp = {}
                                 temp = {child.tag:child.attrib.values()[0]}
-                                self.type = temp
+                                self.types = temp
                     else:
                         if child._children == []:
                             self.length.update({child.tag:child.text})
                         else:
                             if child.tag == 'long':
-                                self.type = child.tag
+                                self.types = child.tag
                                 for gchild in child:
                                     if gchild._children == []:
                                         self.values.update({gchild.tag:gchild.text})
@@ -281,7 +307,7 @@ class DataType:
         else:
             temp = {}
             temp = {self.attr.tag:self.attr.attrib.values()[0]}
-            self.type = temp
+            self.types = temp
             if self.attr._children == []: pass
             else:
                 for child in self.attr:
@@ -301,10 +327,11 @@ if __name__ == '__main__':
     #name = "sample.xml"
     parser = MomParser(name)
 #     parser.mom()
-    parser.mom(mo='Rcs')
+#     parser.mom(mo='celltdd')
 #     parser.mom(mo='Rcs',attr='t')
 #     parser.mom(mo='ReportConfigA1Sec')
 #     parser.mom(mo='utrancelltdd')
 #     parser.mom(mo='utrancelltdd', attr='pmradio')
-#     parser.mom(mo='ReportConfigA1Sec', attr='r')
+#     parser.mom(attr='pmradio')
+    parser.mom(mo='ReportConfigA1Sec', attr='r')
 
