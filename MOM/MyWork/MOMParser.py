f5fc703775d64e1eae637fbfa23ceda8cad559d6
echaseu <echaseu@KR00200510.ericsson.se>
2018-04-04 18:01:55 +0900

    180404 update print

--- MOM/MyWork/MOMParser.py
+++ MOM/MyWork/MOMParser.py
@@ -12,6 +12,8 @@ class MomParser:
         self.smembers = {}
         self.exceps = {}
         self.exmembers = {}
+        self.mim = {}
+        self.relation = {}
         self.__run()
         
     def __run(self):
@@ -29,8 +31,15 @@ class MomParser:
                 elif elem.tag == 'exception': 
                     excepname = MyException(elem)
                     self.exceps[excepname.getName()] = excepname
-                else: pass    
-
+                elif elem.tag == 'mim': # mim version
+                    self.mim = elem.attrib
+                else: 
+                    if elem.tag == 'relationship':
+                        str = elem.attrib.values()[0]
+                        str = str.split('_to_')
+                        str= {str[0]:str[1]}
+                        self.relation.update(str)
+                        
             elif event == 'end': # add infomation in obj
                 for attr in elem: 
                     if elem.tag == 'class' and attr.tag == 'attribute':
@@ -61,14 +70,21 @@ class MomParser:
             else: pass
 
     def mom(self, mo="", attr=""): # print element
-        str = "*" * 200
+        line = "*" * 200
+        print line
+        print 'name:"%s"' % self.mim['name'], 'version:"%s"' % self.mim['version'], 'release:"%s"' %self.mim['release'], 'author:"%s"' %self.mim['author'], 'revision:"%s"' %self.mim['revision']
         if mo == "":    
-            print str,'\n',"MO".ljust(30),'\n',str
-            for mo in self.mos.keys():
-                print mo   
+            # show mo, enum, struct, exception
+            print line,'\n',"MO".ljust(30),'\n',line
+            for mo in self.mos:
+                print mo
+            print '\n', line,'\n',"ENUM".ljust(30),'\n',line
+            for enum in self.enums:
+                print enum
+               
             if attr != "":
-                print str,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'defaultValue'.ljust(30), 'Flags'.ljust(30), 'Range'.ljust(30)
-                print str
+                print line,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'defaultValue'.ljust(30), 'Flags'.ljust(30), 'Range'.ljust(30)
+                print line
                 p = re.compile(attr, re.IGNORECASE)
                 for attr_name in self.attrs:
                     check = p.search(attr)
@@ -88,15 +104,15 @@ class MomParser:
                     if attr == "":
                         getMo.showMoInfo()
                     else:
-                        print str,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'Flags'.ljust(30), 'length'.ljust(10), 'default'.ljust(10), 'Range'
-                        print str
+                        print line,'\n',"MOC".ljust(30), 'Attribute'.ljust(30), 'Flags'.ljust(40), 'length'.ljust(20), 'default'.ljust(20), 'Range'
+                        print line
                         m = re.compile(attr, re.IGNORECASE)
                         attr_list = getMo.getAttrs()
                         for attr_name in sorted(attr_list):
                             check1 = m.search(attr_name)
                             if check1:
                                 getAttr = attr_list[attr_name]
-                                print getAttr.mo.ljust(30), getAttr.getName().ljust(30), getAttr.getFlags().ljust(30), getAttr.getLength().ljust(10), getAttr.getValues().ljust(10), getAttr.getRange()
+                                print getAttr.mo.ljust(30), getAttr.getName().ljust(30), getAttr.getFlags().ljust(40), getAttr.getLength().ljust(20), getAttr.getValues().ljust(20), getAttr.getRange()
                             else:
                                 pass
                 else: pass
@@ -108,6 +124,8 @@ class Mo:
         self.attrs_info = {}
         self.flags = []
         self.others = {}
+        self.parent = None
+        self.child = None
         self.obj = elem
         self.handle()
     
@@ -120,7 +138,7 @@ class Mo:
     def addAttrs(self, attr):
         self.attrs_obj[attr.getName()] = attr
     
-    def getAttrs(self): #get attrs
+    def getAttrs(self): 
         return self.attrs_obj
     
     def addAttrsInfo(self, attr):
@@ -128,6 +146,18 @@ class Mo:
     
     def getAttrsInfo(self):
         return self.attrs_info
+    
+    def addParent(self, name):
+        self.parent = name
+    
+    def addChild(self, name):
+        self.child, name
+    
+    def getParent(self):
+        return self.parent
+    
+    def getChild(self):
+        return self.child
     
     def handle(self):
         for mo_child in self.obj:
@@ -144,13 +174,13 @@ class Mo:
             else: pass
     
     def showMoInfo(self):
-        str = "*" * 120
-        print str,'\n', "MOC", '\n', str 
+        line = "*" * 120
+        print line,'\n', "MOC", '\n', line
         print self.name
         for flag in sorted(self.flags): print flag
         other_list = self.others.keys()
         for key in sorted(other_list): print "%s\t%s" %(key, self.others[key]) 
-        print str,'\n',"Attribute".ljust(30), "Type".ljust(40), 'Flags', '\n', str
+        print line,'\n',"Attribute".ljust(30), "Type".ljust(40), 'Flags', '\n', line
         for key, value in sorted(self.attrs_obj.items()): 
             print key.ljust(30), value.getType().ljust(40), value.getFlags()
         
@@ -174,9 +204,11 @@ class Attr:
         return self.mo
         
     def getFlags(self):
-        self.flags.pop()
-        str = ','.join(self.flags)
-        return str
+        temp = sorted(self.flags)
+        if temp[0] == []:
+            del temp[0]
+        temp = ','.join(temp)
+        return temp
     
     def getType(self):
         if self.types == {}:
@@ -186,18 +218,29 @@ class Attr:
     def getLength(self):
         if self.length == {}:
             return ''
-        else: return str(self.length)
-    
-    def getRange(self):
-        if self.range == {}:
-            return ''
         else: 
-            return str(self.range)
+            leng = []
+            for val in sorted(self.length.keys(),reverse=True):
+                leng.append(int(self.length[val]))
+            return str(leng)
+         
+    def getRange(self):
+        if self.range == []:
+            return ''
+        else:
+            return sorted(self.range)
     
     def getValues(self):
         if self.values == {}:
             return ''
-        else: return str(self.values)
+        else:
+            val = []
+            for v in sorted(self.values.keys()):
+                val.append(self.values[v])
+            return str(val)
+    
+    def getOthers(self):
+        return self.others
     
     def getAttrFlag(self):
         return sorted(self.flags)
@@ -216,18 +259,22 @@ class Attr:
                         data = DataType(child)
                         self.flags.append(data.getFlags())
                         self.length = data.getLength()
-                        self.range = data.getRange()
+#                         self.range = data.getRange()
+                        self.range = data.getRanges()
                         self.values = data.getValues()
                         self.types = data.getType()
                 else:
                     data = DataType(attr)  
                     self.flags.append(data.getFlags())
                     self.length = data.getLength()
-                    self.range = data.getRange()
+#                     self.range = data.getRange()
+                    self.ranges = data.getRanges()
                     self.values = data.getValues()
                     self.types = data.getType()
-                
-        
+                    
+        if 'visibility' in self.others:
+                    self.flags.append('EricssonOnly')
+                    
 class DataType:
     def __init__(self, attr):
         self.attr = attr
@@ -235,6 +282,7 @@ class DataType:
         self.types = None
         self.length = {}
         self.range = {}
+        self.ranges = []
         self.values = {}
         self.__handle()
     
@@ -252,6 +300,9 @@ class DataType:
     
     def getRange(self):
         return self.range
+    
+    def getRanges(self):
+        return self.ranges
     
     def getValues(self):
         return self.values
@@ -283,6 +334,7 @@ class DataType:
                                     else:
                                         for ggchild in gchild:
                                             self.range.update({ggchild.tag:ggchild.text})
+                                            self.ranges.append(int(ggchild.text))
                             else: 
                                 temp = []
                                 for gchild in child:
@@ -294,6 +346,7 @@ class DataType:
                         if child.tag == 'range':
                             for key in child:
                                 self.range.update({key.tag:key.text})
+                                self.ranges.append(int(key.text))
                         elif child.tag == 'lengthRange':
                             for key in child:
                                 self.length.update({key.tag:key.text})
@@ -316,22 +369,16 @@ class DataType:
 class Enum(Mo): pass
 class Struct(Mo): pass
 class MyException(Mo): pass
-
-                       
+ 
 if __name__ == '__main__':
     name = "LteRbsNodeComplete_Itr27_R10D03.xml"
-#     old_mom = """
-#     fdsafsa
-#     
-#     """
     #name = "sample.xml"
     parser = MomParser(name)
 #     parser.mom()
 #     parser.mom(mo='celltdd')
+#     parser.mom(mo='celltdd', attr='pm')
+#     parser.mom(mo='utrancelltdd', attr='pmradio')
 #     parser.mom(mo='Rcs',attr='t')
 #     parser.mom(mo='ReportConfigA1Sec')
-#     parser.mom(mo='utrancelltdd')
-#     parser.mom(mo='utrancelltdd', attr='pmradio')
 #     parser.mom(attr='pmradio')
-    parser.mom(mo='ReportConfigA1Sec', attr='r')
-
+#     parser.mom(mo='ReportConfigA1Sec', attr='r')
