65a13fe75aeebcd079dfc0b61ee1b75e6e37bf56
echaseu <echaseu@KR00200021.ericsson.se>
2018-04-25 17:17:59 +0900

    add difflib test

--- training/xml/test.py
+++ training/xml/test.py
@@ -1,41 +1,37 @@
-#!/usr/bin/python
-from xml.etree.ElementTree import Element, SubElement, dump
-from xml.etree.ElementTree import ElementTree
-from xml.etree.ElementTree import parse
+import difflib
 
-note = Element("note")
-note.attrib["date"] = "20120104"
+text1 = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer
+eu lacus accumsan arcu fermentum euismod. Donec pulvinar porttitor
+tellus. Aliquam venenatis. Donec facilisis pharetra tortor.  In nec
+mauris eget magna consequat convallis. Nam sed sem vitae odio
+pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
+metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
+urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
+suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
+adipiscing. Suspendisse eu lectus. In nunc. Duis vulputate tristique
+enim. Donec quis lectus a justo imperdiet tempus."""
+text1_lines = text1.splitlines()
 
-to = Element("to")
-to.text = "Tove"
-note.append(to)
+text2 = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer
+eu lacus accumsan arcu fermentum euismod. Donec pulvinar, porttitor
+tellus. Aliquam venenatis. Donec facilisis pharetra tortor. In nec
+mauris eget magna consequat convallis. Nam cras vitae mi vitae odio
+pellentesque interdum. Sed consequat viverra nisl. Suspendisse arcu
+metus, blandit quis, rhoncus ac, pharetra eget, velit. Mauris
+urna. Morbi nonummy molestie orci. Praesent nisi elit, fringilla ac,
+suscipit non, tristique vel, mauris. Curabitur vel lorem id nisl porta
+adipiscing. Duis vulputate tristique enim. Donec quis lectus a justo
+imperdiet tempus. Suspendisse eu lectus. In nunc. """
+text2_lines = text2.splitlines()
 
-SubElement(note, "from").text = "Jani"
-SubElement(note, "heading").text = "Reminder"
-SubElement(note, "body").text = "Don't forget me this weekend!"
-
-def indent(elem, level=0):
-	i = "\n" + level*"  "
-	if len(elem):
-		if not elem.text or not elem.text.strip():
-			elem.text = i + "  "
-		if not elem.tail or not elem.tail.strip():
-			elem.tail = i
-		for elem in elem:
-			indent(elem, level+1)
-		if not elem.tail or not elem.tail.strip():
-			elem.tail = i
-	else:
-	 	if level and (not elem.tail or not elem.tail.strip()):
-			elem.tail = i
-indent(note)
-dump(note)
-
-#ElementTree(note).write("note.xml")
-tree = parse("note.xml")
-note = tree.getroot()
-
-#print note.get("date")
-#print note.get("foo", "default")
-#print note.keys()
-#print note.items()
+d = difflib.Differ()
+diff = d.compare(text1_lines, text2_lines)
+print '\n'.join(diff)
+print '\n'
+print '\n'
+diff = difflib.ndiff(text1_lines, text2_lines)
+print '\n'.join(list(diff))
+print '\n'
+print '\n'
+diff = difflib.unified_diff(text1_lines, text2_lines, lineterm='')
+print '\n'.join(list(diff))
